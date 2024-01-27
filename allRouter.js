39beda16cd076998dfe63dca56b9868c3c917bbb
Duchnh <hduy01012000@gmail.com>
2024-01-27 12:25:19 +0700

    TICKET INIT SWAGGER

--- allRouter.js
+++ allRouter.js
@@ -8,7 +8,26 @@ import * as shopController from './APP/Controller/ShopController.js';
 const router = express.Router();
 // ADMIN SECTION
 router.post('/api/admin/login', admController.loginAdmin);
+
 // PRODUCT SECTION
+/**
+ * @swagger
+ * /api/product:
+ *   get:
+ *     summary: Get all products
+ *     description: Retrieve a list of all products.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Success
+ *               data:
+ *                 products: [...]
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/api/product', shopController.getAllProduct);
 
 export default router;
--- swagger.js
+++ swagger.js
@@ -6,7 +6,16 @@ const options = {
       version: '1.0.0',
       description: 'This is a swagger API',
     },
-    servers: [{ url: 'http://localhost:3500', description: 'This is the local development environment' }],
+    servers: [
+      {
+        url: 'http://localhost:3500',
+        description: 'This is the local development environment',
+      },
+      {
+        url: 'https://pc-geared-capstone-com.onrender.com',
+        description: 'This is the cloud development environment',
+      },
+    ],
     components: {
       securitySchemes: {
         BearerAuth: {
@@ -20,8 +29,8 @@ const options = {
   apis: ['./allRouter.js'],
   security: [
     {
-      BearerAuth: [], 
+      BearerAuth: [],
     },
-  ]
+  ],
 };
 export default options;

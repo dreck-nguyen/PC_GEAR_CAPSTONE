97c18902be0dfcf603a93169db2049d2bec9baa8
Duchnh <hduy01012000@gmail.com>
2024-02-20 21:50:21 +0700

    Ticket Refractor

--- APP/Service/ProductService.js
+++ APP/Service/ProductService.js
@@ -1,7 +1,6 @@
 import * as productDAL from '../DAL/ProductDAL.js';
 export async function getAllProduct() {
   const productsWithDetails = await productDAL.getAllProduct();
-  console.log(productsWithDetails);
   return productsWithDetails;
 }
 export async function getProductsByCategory(categoryId) {

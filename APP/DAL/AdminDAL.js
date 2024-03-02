ea8ba8e9d527b7ece7daed8ec5f5277d9eae237a
Duchnh <hduy01012000@gmail.com>
2024-03-02 16:28:17 +0700

    Ticket Update : update admin get list user

--- APP/DAL/AdminDAL.js
+++ APP/DAL/AdminDAL.js
@@ -1,4 +1,4 @@
-import { User, UserRole } from '../utility/DbHelper.js';
+import { SequelizeInstance, User, UserRole } from '../utility/DbHelper.js';
 
 export async function getAdmDetails(email) {
   const adminDetails = await User.findOne({
@@ -19,16 +19,15 @@ export async function getAdmDetails(email) {
 }
 
 export async function getUsers() {
-  const userList = await User.findAll({
-    include: [
-      {
-        model: UserRole,
-        as: 'userRole',
-        where: {
-          role: 'USER',
-        },
-      },
-    ],
+  const sqlQuery = `
+  select * from public."user" u 
+  inner join user_role ur 
+  on u.role_id  = ur.role_id 
+  where ur."role" != 'ADMIN'
+`;
+  const userList = await SequelizeInstance.query(sqlQuery, {
+    type: SequelizeInstance.QueryTypes.SELECT,
+    raw: true,
   });
   return userList;
 }

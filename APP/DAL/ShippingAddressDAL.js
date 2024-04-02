61e28ceb8c9abf7113cf440ae331219f9bc985ce
Duchnh <hduy01012000@gmail.com>
2024-04-02 21:58:50 +0700

    Update logic shipping address

--- APP/DAL/ShippingAddressDAL.js
+++ APP/DAL/ShippingAddressDAL.js
@@ -1,49 +1,106 @@
-import { ShippingAddress } from '../utility/DbHelper.js';
+import { SequelizeInstance, ShippingAddress } from '../utility/DbHelper.js';
 
 export async function getShippingAddress(userId) {
-  const result = await ShippingAddress.findAll({ where: { user_id: userId } });
+  const sqlQuery = `
+SELECT 
+   *
+FROM 
+  shipping_address
+WHERE
+  user_id = '${userId}'
+`;
+
+  const result = await SequelizeInstance.query(sqlQuery, {
+    type: SequelizeInstance.QueryTypes.SELECT,
+    raw: true,
+  });
+
   return result;
 }
 
 export async function createShippingAddress(userId, dataObj) {
-  const result = await ShippingAddress.create({
-    address_id: dataObj.address_id,
-    user_id: userId,
-    recipient_name: dataObj.recipient_name,
-    street_address: dataObj.street_address,
-    city: dataObj.city,
-    country: dataObj.country,
+  const {
+    address_id,
+    recipient_name,
+    street_address,
+    city,
+    recipient_phone,
+    district,
+    ward,
+  } = dataObj;
+
+  const sqlQuery = `
+    INSERT INTO public.shipping_address 
+    (address_id, user_id, recipient_name, street_address, city, created_at, recipient_phone, district, ward) 
+    VALUES (?, ?, ?, ?, ?, now(), ?, ?, ?)
+    RETURNING *
+  `;
+
+  const result = await SequelizeInstance.query(sqlQuery, {
+    replacements: [
+      address_id,
+      userId,
+      recipient_name,
+      street_address,
+      city,
+      recipient_phone,
+      district,
+      ward,
+    ],
+    type: SequelizeInstance.QueryTypes.INSERT,
   });
+
   return result;
 }
 
 export async function updateShippingAddress(userId, dataObj) {
-  const { address_id, recipient_name, street_address, city, country } = dataObj;
+  const {
+    address_id,
+    recipient_name,
+    street_address,
+    city,
+    recipient_phone,
+    district,
+    ward,
+  } = dataObj;
 
-  const [updatedRows, [updatedShippingAddress]] = await ShippingAddress.update(
-    {
+  const sqlQuery = `
+    UPDATE public.shipping_address 
+    SET recipient_name = ?, 
+        street_address = ?, 
+        city = ?, 
+        recipient_phone = ?, 
+        district = ?, 
+        ward = ? 
+    WHERE address_id = ? AND user_id = ?
+  `;
+
+  const result = await SequelizeInstance.query(sqlQuery, {
+    replacements: [
       recipient_name,
       street_address,
       city,
-      country,
-    },
-    {
-      where: {
-        address_id,
-        user_id: userId,
-      },
-      returning: true,
-    },
-  );
-  return updatedShippingAddress;
-}
-export async function deleteShippingAddress(userId, addressId) {
-  const deletedRows = await ShippingAddress.destroy({
-    where: {
-      user_id: userId,
-      address_id: addressId,
-    },
+      recipient_phone,
+      district,
+      ward,
+      address_id,
+      userId,
+    ],
+    type: SequelizeInstance.QueryTypes.UPDATE,
   });
 
-  return deletedRows;
+  return result;
+}
+export async function deleteShippingAddress(userId, addressId) {
+  const sqlQuery = `
+    DELETE FROM shipping_address
+    WHERE address_id = ? AND user_id = ?
+  `;
+
+  const result = await SequelizeInstance.query(sqlQuery, {
+    replacements: [addressId, userId],
+    type: SequelizeInstance.QueryTypes.UPDATE,
+  });
+
+  return result;
 }

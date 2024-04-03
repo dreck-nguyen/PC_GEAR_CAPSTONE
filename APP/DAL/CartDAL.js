782f6997dce5a946d0ebf2c20b54f19d1f557836
Duchnh <hduy01012000@gmail.com>
2024-04-03 21:17:52 +0700

    Format sql

--- APP/DAL/CartDAL.js
+++ APP/DAL/CartDAL.js
@@ -2,49 +2,70 @@ import { Cart, SequelizeInstance } from '../utility/DbHelper.js';
 
 export async function getCartUser(userId) {
   const sqlQuery = `
- select 
-  c.*,
-  ci.*,
-    JSONB_BUILD_OBJECT(
-      'cart_item_id', ci.cart_item_id,
-      'product_id', ci.product_id,
-      'product_name', p.name,
-      'quantity', ci.quantity,
-      'unit_price', TO_CHAR(p.unit_price, 'FM999,999,999'),
-      'created_at', ci.created_at,
-      'images', pg.images
-    ) AS product_detail,
-  sum(ci.quantity) as product_total,
-  ci.personal_build_pc_id,
-  to_json(upb.*) as build_pc_details 
-from 
-  cart c 
+ select
+	c.*,
+	JSONB_BUILD_OBJECT(
+      'cart_item_id',
+	ci.cart_item_id,
+	'product_id',
+	ci.product_id,
+	'product_name',
+	p.name,
+	'quantity',
+	ci.quantity,
+	'unit_price',
+	TO_CHAR(p.unit_price,
+	'FM999,999,999'),
+	'created_at',
+	ci.created_at,
+	'images',
+	pg.images
+    ) as product_detail,
+	sum(ci.quantity) as product_total,
+	ci.personal_build_pc_id,
+	to_json(upb.*) as build_pc_details
+from
+	cart c
 left join 
   cart_item ci 
-  on 1=1
-  and c.cart_id = ci.cart_id
+  on
+	1 = 1
+	and c.cart_id = ci.cart_id
 left join 
 product p
-  on 1=1
-  and ci.product_id = p.product_id
+  on
+	1 = 1
+	and ci.product_id = p.product_id
 left join 
-  (select 
-    product_id 
-    , ARRAY_AGG(image) as images 
-    from 
-      product_gallery
-    group by 
-      product_id) pg
-  on 1=1 
-  and pg.product_id = p.product_id
+  (
+	select
+		product_id 
+    ,
+		ARRAY_AGG(image) as images
+	from
+		product_gallery
+	group by
+		product_id) pg
+  on
+	1 = 1
+	and pg.product_id = p.product_id
 left join
   user_pc_build upb 
-  on 1=1 
-  and upb.user_pc_build_id  = ci.personal_build_pc_id 
-where 1=1 
-and c.user_id = '${userId}'
-group by 
-  c.cart_id,ci.personal_build_pc_id,upb.*,ci.*,ci.cart_item_id,p.name,p.unit_price,pg.images
+  on
+	1 = 1
+	and upb.user_pc_build_id = ci.personal_build_pc_id
+where
+	1 = 1
+	and c.user_id = '${userId}'
+group by
+	c.cart_id,
+	ci.personal_build_pc_id,
+	upb.*,
+	ci.*,
+	ci.cart_item_id,
+	p.name,
+	p.unit_price,
+	pg.images
 `;
 
   const userCart = await SequelizeInstance.query(sqlQuery, {

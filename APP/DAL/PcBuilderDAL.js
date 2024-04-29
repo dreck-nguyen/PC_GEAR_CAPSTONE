05384854ea59db91fe541abacf22e5288a55680a
Duchnh <hduy01012000@gmail.com>
2024-04-30 01:20:02 +0700

    Modify response

--- APP/DAL/PcBuilderDAL.js
+++ APP/DAL/PcBuilderDAL.js
@@ -1520,7 +1520,22 @@ left join
     c."name" as category_name,
     pb.product_brand_name as brand_name,
     ARRAY_AGG(pg.image) as image_links,
-    gs.*
+     gs.specification_id
+  , gs.product_id
+  , gs.product_specification_type
+  , gs.brand
+  , gs.chipset
+  , gs.memory
+  , gs.benchmark
+  , gs.max_power_consumption
+  , gs.base_clock_speed
+  , gs.length
+  , gs.cooler_type
+  , gs.interface
+  , gs."VRAM"
+  , gi.interface_type as gpu_interface
+  , gs.gpu_version
+  , gs.gpu_physical_size
   from
     product p
   left outer join category c on
@@ -1531,12 +1546,14 @@ left join
     pg.product_id = p.product_id
   inner join graphics_specification gs on
     p.product_id = gs.product_id
+  inner join graphics_interface gi 
+  on gi.id = gs.gpu_interface 
   group by
     p.product_id,
     c.category_id,
     pb.product_brand_id,
     gs.product_id,
-    gs.specification_id
+    gs.specification_id, gi.interface_type
 ) gs 
 on
   upb.gpu_id = gs.primary_product_id
